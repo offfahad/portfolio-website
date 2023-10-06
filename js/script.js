83e49d5deedbd6ed63348c0e4cc9b1ea3101c68f
offfahad <mughalfahad544@gmail.com>
2023-10-06 20:45:25 +0500

    typing-effect-added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function runTypingEffect(){
+    const text = 'I am Fahad.'
+    const typingElement = document.getElementById('typing-text');
+    const typingDelay = 200;
+
+    typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay){
+    for (let index = 0; index < text.length; index++) {
+        setTimeout(() => {
+          typingElement.textContent += text.charAt(index);  
+        }, delay * index);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
